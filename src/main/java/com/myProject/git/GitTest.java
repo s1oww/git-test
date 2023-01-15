3ac4fcda5a51d90b0347fab69e31a0b3cf961c6d
96485 <964851874@qq.com>
2023-01-15 21:25:16 +0800

    commit 3

--- src/main/java/com/myProject/git/GitTest.java
+++ src/main/java/com/myProject/git/GitTest.java
@@ -1,16 +1,17 @@
 package com.myProject.git;
 
 /**
- * @Description
- * @Pcakage: com.myProject.git.GitTest
- * @className: GitTest
- * @Date 2023/1/15 20:53
- * @Version 1.0
+ * @description
+ * @package com.myProject.git.GitTest
+ * @className GitTest
+ * @date 2023/1/15 20:53
+ * @version 1.0
  * @since 1.0
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git! 1");
+        System.out.println("hello git! 2");
     }
 }
