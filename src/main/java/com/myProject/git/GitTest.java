50aeeb5a1cade5d4d5d5107ea826084948f416c1
96485 <964851874@qq.com>
2023-01-15 21:10:00 +0800

    commit 1

---
+++ src/main/java/com/myProject/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.myProject.git;
+
+/**
+ * @Description
+ * @Pcakage: com.myProject.git.GitTest
+ * @className: GitTest
+ * @Date 2023/1/15 20:53
+ * @Version 1.0
+ * @since 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
