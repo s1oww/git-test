b1354cc58d86d420f3e2c1a6a351ef74502956de
96485 <964851874@qq.com>
2023-01-15 21:32:24 +0800

    master commit 2

--- src/main/java/com/myProject/git/GitTest.java
+++ src/main/java/com/myProject/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git! 1");
         System.out.println("hello git! 2");
         System.out.println("hello git! 3");
+        System.out.println("master test");
     }
 }
