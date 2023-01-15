d0fc4a0d3a1e3020d5b6be08f13160e27ed6ddbe
96485 <964851874@qq.com>
2023-01-15 21:42:08 +0800

    push commit

--- src/main/java/com/myProject/git/GitTest.java
+++ src/main/java/com/myProject/git/GitTest.java
@@ -16,6 +16,6 @@ public class GitTest {
         System.out.println("hello git! 3");
         System.out.println("master test");
         System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
